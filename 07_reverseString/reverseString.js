cf26296c1b6c82f03c3d2e21815293d9a00eda9c
Antonio <antonio.pulgarin97@gmail.com>
2025-06-17 17:32:48 -0500

    solucion ejercicio 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
 
+
+
+const reverseString = function(var_string) {
+    let respuesta = "";
+for (let i = var_string.length  - 1; i >= 0; i--){
+    respuesta = respuesta + var_string[i];
+    console.log(i);
+}
+return respuesta;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
